e5570a47383400e062be8b98d7e1c7a566c54fd7
Chirag Maheshwari <csoni111@gmail.com>
2018-11-05 03:30:22 +0530

    minor changes

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const sitename = new Sitename;
+let sitename = new Sitename();
 
 let stats = {};
 let domains = {};
 let requestCounts = {};
 
-chrome.runtime.onInstalled.addListener(details => {
-  console.log('on installed called');
+chrome.runtime.onInstalled.addListener(function (details) {
+  console.log('on installed called', details);
 });
 
 let intervalId = setInterval(function () {
@@ -15,8 +15,6 @@ let intervalId = setInterval(function () {
     clearInterval(intervalId);
 
     chrome.tabs.query({}, function (tabs) {
-      let count = tabs.length;
-
       for (let tab in tabs) {
         domains[tab.id] = sitename.get(tab.url);
       }
@@ -25,59 +23,51 @@ let intervalId = setInterval(function () {
 }, 100);
 
 chrome.webRequest.onBeforeRequest.addListener(function (details) {
-  const tabId = details.tabId;
-  const isParent = details.type === 'main_frame';
-  const requestedUrl = details.url;
-  const childDomain = sitename.get(requestedUrl);
+  let tabId = details.tabId;
+  let isParent = details.type === 'main_frame';
+  let requestedUrl = details.url;
+  let childDomain = sitename.get(requestedUrl);
   if (isParent) {
     domains[tabId] = childDomain;
   }
-  const childDomainDetails = blacklist[childDomain];
-  const parentDomain = domains[tabId];
+  let childDomainDetails = blacklist[childDomain];
+  let parentDomain = domains[tabId];
 
-  let blockingResponse = {cancel: false};
-  let tabStats = stats[tabId] || (stats[tabId] = {total: 0, blocked: 0});
+  let blockingResponse = { cancel: false };
+  let tabStats = stats[tabId] || (stats[tabId] = { total: 0, blocked: 0 });
   let totalCount = ++tabStats.total;
   if (childDomainDetails) {
-    const parentDomainDetails = blacklist[parentDomain];
-    const isContent = childDomainDetails.category === 'content';
-    if (
-      tabId === -1 || isParent || !parentDomain || childDomain === parentDomain
-      || parentDomainDetails && childDomainDetails.name === parentDomainDetails.name
-    ) { // The request is allowed: the topmost frame has the same origin.
-    }
+    let parentDomainDetails = blacklist[parentDomain];
+    let isContent = childDomainDetails.category === 'content';
+    if (tabId === -1 || isParent || !parentDomain || childDomain === parentDomain || parentDomainDetails && childDomainDetails.name === parentDomainDetails.name) {} // The request is allowed: the topmost frame has the same origin.
+
     // else if (!isContent) { // The request is allowed
     // }
     else {
+      console.log('blocked', requestedUrl);
       blockingResponse = {
-        redirectUrl:
-          details.type === 'image' ?
-            'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAACklEQVR4nGMAAQAABQABDQottAAAAABJRU5ErkJggg=='
-            : 'about:blank'
+        redirectUrl: details.type === 'image' ? 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAACklEQVR4nGMAAQAABQABDQottAAAAABJRU5ErkJggg==' : 'about:blank'
       };
       let blockedCount = ++tabStats.blocked;
     } // The request is denied.
 
-    if (blockingResponse.redirectUrl)
-      incrementCounter(tabId, childDomainDetails, isContent);
+    if (blockingResponse.redirectUrl) incrementCounter(tabId, childDomainDetails, isContent);
   }
   return blockingResponse;
-}, {urls: ['http://*/*', 'https://*/*']}, ['blocking']);
-
+}, { urls: ['http://*/*', 'https://*/*'] }, ['blocking']);
 
 function incrementCounter(tabId, domainDetails, blocked) {
   let tabRequests = requestCounts[tabId] || (requestCounts[tabId] = {});
   let cat = domainDetails.category;
   let catRequests = tabRequests[cat] || (tabRequests[cat] = {});
-  let domainRequests = catRequests[domainDetails.name] ||
-    (catRequests[domainDetails.name] = {url: domainDetails.url, count: 0});
+  let domainRequests = catRequests[domainDetails.name] || (catRequests[domainDetails.name] = { url: domainDetails.url, count: 0 });
   ++domainRequests.count;
   safelyUpdateCounter(tabId, getCount(tabRequests), !blocked);
 }
 
 function safelyUpdateCounter(tabId, count, deactivated) {
   chrome.tabs.query({}, function (tabs) {
-    for (var i = 0; i < tabs.length; i++) {
+    for (let i = 0; i < tabs.length; i++) {
       if (tabId === tabs[i].id) {
         updateCounter(tabId, count, deactivated);
         break;
@@ -90,11 +80,12 @@ function getCount(tabRequests) {
   let count = 0;
   for (let categoryName in tabRequests) {
     let category = tabRequests[categoryName];
-    for (let domainName in category) count += category[domainName].count;
+    for (let domainName in category) {
+      count += category[domainName].count;
+    }
   }
   return count;
 }
-
 
 function updateCounter(tabId, count, deactivated) {
   chrome.browserAction.setBadgeText({
