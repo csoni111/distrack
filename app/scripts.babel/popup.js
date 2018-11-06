b7c4b1d98320dd31d74352fd1ee303a3a043ee7c
Chirag Maheshwari <csoni111@gmail.com>
2018-11-06 19:07:58 +0530

    add stats counter in popup

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,3 +1,12 @@
 'use strict';
+const bg = chrome.extension.getBackgroundPage();
+const tabStats = bg.stats;
+const requestCounts = bg.requestCounts;
 
-console.log('\'Allo \'Allo! Popup');
+$(document).ready(() => {
+  chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+    let currTab = tabs[0];
+    $('#request-count #total').text(tabStats[currTab.id].total);
+    $('#request-count #blocked').text(tabStats[currTab.id].blocked);
+  });
+});
